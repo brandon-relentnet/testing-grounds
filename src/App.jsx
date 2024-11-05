7987c29cec0778e6d00b21cdf19430f1273e2a8c
Brandon Harris <brandon@relentnet.com>
2024-11-04 18:14:55 -0600

    added files

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Theme from './features/theme/Theme';
+import Settings from './pages/Settings';
+import Accent from './features/accent/Accent';
+import FontFamily from './features/font-family/FontFamily';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <Router>
+      <Theme /> 
+      <Accent />
+      <FontFamily />
+      
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
